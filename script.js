bd83ee19afe02cc7e4ae641cfb6b248e2a2a0de1
Anderson9683 <112121272+Anderson9683@users.noreply.github.com>
2026-05-02 20:31:18 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(){
-
 const produtosDiv = document.getElementById("produtos");
 
-if(!produtosDiv) return;
+if(produtosDiv){
 
-// LINK DO SEU GOOGLE SHEETS
 fetch("https://docs.google.com/spreadsheets/d/1OAA883uAREpPG-h7pupwizHH8WAEsIp3rHxbuGlIsrA/export?format=csv")
-
 .then(res => res.text())
-
 .then(data => {
 
 const linhas = data.split("\n").slice(1);
@@ -24,8 +19,6 @@ const preco = colunas[1];
 const imagem = colunas[2];
 const descricao = colunas[3];
 
-if(!nome) return;
-
 produtosDiv.innerHTML += `
 <div class="produto">
 <img src="${imagem}">
@@ -33,7 +26,7 @@ produtosDiv.innerHTML += `
 <p>R$ ${preco}</p>
 <p>${descricao}</p>
 
-<a href="https://wa.me/5551998084487?text=Olá quero comprar ${nome}">
+<a href="https://wa.me/5551998084487?text=Olá quero comprar ${nome}" target="_blank">
 <button>Comprar</button>
 </a>
 
@@ -43,5 +36,4 @@ produtosDiv.innerHTML += `
 });
 
 });
-
-});
+}
