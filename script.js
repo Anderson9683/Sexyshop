1a88a2eda0ca545559066bd619439cc5466e680a
Anderson9683 <112121272+Anderson9683@users.noreply.github.com>
2026-05-01 22:14:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function comprar(produto){
-alert("Produto adicionado ao carrinho: " + produto);
+console.log("Valendo Sexy Shop Online 🔥");
+function comprar(){
+
+let numero = "51998084487";
+
+let mensagem = "Olá! Quero comprar um produto do Valendo Sexy Shop 🔥";
+
+window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`);
+
 }
