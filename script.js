87f1373eccb9b735927b189f30fddb6fe2b9721d
Anderson9683 <112121272+Anderson9683@users.noreply.github.com>
2026-05-02 18:51:30 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-console.log("🔥 Valendo Sexy Shop Online");
-
-// Espera página carregar
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function(){
 
 const produtosDiv = document.getElementById("produtos");
 
-if (!produtosDiv) return;
+if(!produtosDiv) return;
 
-// LINK CSV DA PLANILHA
-const url =
-"https://docs.google.com/spreadsheets/d/1OAA883uAREpPG-h7pupwizHH8WAEsIp3rHxbuGlIsrA/export?format=csv";
+// LINK DO SEU GOOGLE SHEETS
+fetch("https://docs.google.com/spreadsheets/d/1OAA883uAREpPG-h7pupwizHH8WAEsIp3rHxbuGlIsrA/export?format=csv")
 
-// Buscar produtos
-fetch(url)
 .then(res => res.text())
+
 .then(data => {
 
 const linhas = data.split("\n").slice(1);
@@ -22,30 +17,25 @@ produtosDiv.innerHTML = "";
 
 linhas.forEach(linha => {
 
-if(!linha.trim()) return;
-
-// IMPORTANTE → separado por vírgula
 const colunas = linha.split(",");
 
 const nome = colunas[0];
 const preco = colunas[1];
 const imagem = colunas[2];
-const descricao = colunas[3] || "";
+const descricao = colunas[3];
+
+if(!nome) return;
 
 produtosDiv.innerHTML += `
 <div class="produto">
-
-<img src="${imagem}" alt="${nome}">
-
+<img src="${imagem}">
 <h3>${nome}</h3>
-
 <p>R$ ${preco}</p>
-
 <p>${descricao}</p>
 
-<button onclick="comprar('${nome}')">
-Comprar no WhatsApp
-</button>
+<a href="https://wa.me/5551998084487?text=Olá quero comprar ${nome}">
+<button>Comprar</button>
+</a>
 
 </div>
 `;
@@ -53,18 +43,5 @@ Comprar no WhatsApp
 });
 
 });
-
-// BOTÃO COMPRAR
-window.comprar = function(produto){
-
-const numero = "5551998084487";
-
-const msg = `Olá! Quero comprar: ${produto} 🔥`;
-
-window.open(
-`https://wa.me/${numero}?text=${encodeURIComponent(msg)}`
-);
-
-};
 
 });
