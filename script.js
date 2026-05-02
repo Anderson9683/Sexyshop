48cb6ffb9bfcc1c86ffbee9049c6763ce5d159c4
Anderson9683 <112121272+Anderson9683@users.noreply.github.com>
2026-05-02 18:00:04 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-const url =
-"https://docs.google.com/spreadsheets/d/1OAA883uAREpPG-h7pupwizHH8WAEsIp3rHxbuGlIsrA/export?format=csv";
+const produtosDiv = document.getElementById("produtos");
 
-fetch(url)
-  .then(response => response.text())
-  .then(data => {
-    const linhas = data.split("\n").slice(1);
+if(produtosDiv){
 
-    const produtosDiv = document.getElementById("produtos");
+fetch("https://docs.google.com/spreadsheets/d/1OAA883uAREpPG-h7pupwizHH8WAEsIp3rHxbuGlIsrA/export?format=csv")
+.then(res => res.text())
+.then(data => {
 
-    linhas.forEach(linha => {
-      const colunas = linha.split(",");
+const linhas = data.split("\n").slice(1);
 
-      const nome = colunas[0];
-      const preco = colunas[1];
-      const imagem = colunas[2];
-      const descricao = colunas[3];
+produtosDiv.innerHTML = "";
 
-      produtosDiv.innerHTML += `
-        <div class="produto">
-          <img src="${imagem}" width="200">
-          <h3>${nome}</h3>
-          <p>R$ ${preco}</p>
-          <p>${descricao}</p>
-        </div>
-      `;
-    });
-  });
+linhas.forEach(linha => {
+
+const colunas = linha.split(";");
+
+const nome = colunas[0];
+const preco = colunas[1];
+const imagem = colunas[2];
+const descricao = colunas[3];
+
+produtosDiv.innerHTML += `
+<div class="produto">
+<img src="${imagem}">
+<h3>${nome}</h3>
+<p>R$ ${preco}</p>
+<p>${descricao}</p>
+</div>
+`;
+
+});
+
+});
+
+}
