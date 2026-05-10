c091252aa7aba394f9127ceea8edbc7096a40639
Anderson9683 <112121272+Anderson9683@users.noreply.github.com>
2026-05-09 22:24:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const produtosDiv = document.getElementById("produtos");
+document.addEventListener("DOMContentLoaded", function () {
 
-if (produtosDiv) {
+const produtos = [
+{
+nome:"Vibrador Luxo",
+preco:"R$ 89,90",
+img:"https://via.placeholder.com/250"
+},
+{
+nome:"Algemas Sensuais",
+preco:"R$ 39,90",
+img:"https://via.placeholder.com/250"
+},
+{
+nome:"Gel Beijável",
+preco:"R$ 29,90",
+img:"https://via.placeholder.com/250"
+}
+];
 
-fetch("https://docs.google.com/spreadsheets/d/1OAA883uAREpPG-h7pupwizHH8WAEsIp3rHxbuGlIsrA/export?format=csv")
-.then(res => res.text())
-.then(data => {
+const lista = document.getElementById("produtos");
 
-const linhas = data.split("\n").slice(1);
-
-produtosDiv.innerHTML = "";
-
-linhas.forEach(linha => {
-
-if(!linha.trim()) return;
-
-const colunas = linha.split(";");
-
-const nome = colunas[0];
-const preco = colunas[1];
-const imagem = colunas[2];
-const descricao = colunas[3];
-
-produtosDiv.innerHTML += `
-<div class="produto">
-<img src="${imagem}">
-<h3>${nome}</h3>
-<p>R$ ${preco}</p>
-<p>${descricao || ""}</p>
+produtos.forEach(p => {
+const card = `
+<div class="card">
+<img src="${p.img}">
+<h3>${p.nome}</h3>
+<p>${p.preco}</p>
 </div>
 `;
 
+lista.innerHTML += card;
 });
 
 });
-
-}
